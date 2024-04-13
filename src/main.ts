5d156aba2f5b7196e9e90459340e2a292ca13e00
baptr2304 <truongbaophuc456@gmail.com>
2024-04-14 00:20:49 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import Root from './Root.vue'
 import router from './router'
